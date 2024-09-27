f24a71c04539ac5c44dd408f02f6264c7ff24478
Omkar-3003 <omkartambe3003@gmail.com>
2024-09-28 01:46:31 +0530

    cart symbol number

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -59,7 +59,7 @@ const CartItems = () => {
             <hr />
             <div className="cartitems-total-item">
               <h3>Total</h3>
-              <h3>${0}</h3>
+              <h3>${getTotalCartAmount()}</h3>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -25,17 +25,28 @@ const ShopContextProvider = (props) => {
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const Item in cartItems) {
-      if (cartItems[item] > 0) {
+      if (cartItems[Item] > 0) {
         let itemInfo = all_product.find(
-          (product) => product.id === Number(item)
+          (product) => product.id === Number(Item)
         );
-        totalAmount += itemInfo.new_price * cartItems[item];
+        totalAmount += itemInfo.new_price * cartItems[Item];
       }
       return totalAmount;
     }
   };
 
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
   const contextValue = {
+    getTotalCartItems,
     getTotalCartAmount,
     all_product,
     cartItems,
